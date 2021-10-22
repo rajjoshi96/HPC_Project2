fada7e07c8a0ffef0c4314a9a25a216b4c91c441
rajjoshi96 <rajjoshi1501@gmail.com>
2021-10-22 21:49:15 +0800

    Revert "tt"
    This reverts commit a26727590e52590c605237ade0b46871c568f2a0.
    
    perfect#

--- quick_serial.c
+++ quick_serial.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include<sys/time.h>
-#define Size 10
+#define Size 100000
 
 // Swap function
 void swap(float* a, float* b)
@@ -61,36 +61,13 @@ int main(int argc, char *argv[])
     double store_time[8];
     float a = 10.0;
     float arr[Size] = {};
-    // for (int i=0;i<Size;i++)
-    // {
-    //     arr[i] = ((float)rand()/(float)(RAND_MAX)) * a;
-    // }
-    double array[Size]={};
-    float t=10.0;
-    for ( int i = 0 ; i < Size ; i++ ) {
-        array[i] =((double)rand()/(double)(RAND_MAX)) * t;
+    for (int i=0;i<Size;i++)
+    {
+        arr[i] = ((float)rand()/(float)(RAND_MAX)) * a;
     }
-
-    // write it
-    FILE *fp = fopen("testfile1.bin","wb");
-    // for ( int i = 0 ; i < Size ; i++ ) {
-    //     fwrite(&array[i],sizeof(array[i]),1,fp);
-    // }
-
-    // fp = fopen("unsorted_array.bin","wb");
-    for ( int i = 0 ; i < Size ; i++ ) {
-        fwrite(&array[i],sizeof(array[i]),1,fp);
-    }
-    fclose(fp);
-    float input[Size]={};
-    fp = fopen("unsorted_array.bin","rb");
-    for ( int i = 0 ; i < Size ; i++ ) {
-        fread(&input[i],sizeof(input[i]),1,fp);
-    }
-    fclose(fp);
-    quickSort(input, 0, Size-1);
+    quickSort(arr, 0, Size-1);
     printf("The sorted array is: ");
-    printArray(input, Size);
+    printArray(arr, Size);
     gettimeofday(&end, NULL);
     double delta = ((end.tv_sec  - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
     printf("Delta = %f \n",delta);  
